f0062e539cad088320cfc2b15c02ba3735b4a103
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-20 15:58:52 -0600

    Create spec test for 'hit' and 'sunk?' methods

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -10,6 +10,31 @@ RSpec.describe Ship do
       expect(@cruiser).to be_a(Ship)
       expect(@cruiser.name).to eq("Cruiser")
       expect(@cruiser.length).to eq(3)
+      expect(@cruiser.health).to eq(3)
+      expect(@cruiser.sunk?).to be(false)
     end
+  end
+
+  describe "#hit" do
+    it "hits the ship, takes health, and sinks it if health is all gone" do
+      expect(@cruiser.health).to eq(3)
+      expect(@cruiser.sunk?).to be(false)
+
+      @cruiser.hit
+
+      expect(@cruiser.health).to eq(2)
+      expect(@cruiser.sunk?).to be(false)
+
+      @cruiser.hit
+
+      expect(@cruiser.health).to eq(1)
+      expect(@cruiser.sunk?).to be(false)
+
+      @cruiser.hit
+
+      expect(@cruiser.health).to eq(0)
+      expect(@cruiser.sunk?).to be(true)
+    end
+
   end
 end
