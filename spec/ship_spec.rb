7c1d51d8434346a59238796711ac609decf72a51
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-20 15:47:58 -0600

    Create basic attribute test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,1 +1,15 @@
 require './lib/ship'
+
+RSpec.describe Ship do
+  before(:each) do
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+  
+  describe "#initialize" do
+    it "exists and its attributes exist" do
+      expect(@cruiser).to be_a(Ship)
+      expect(@cruiser.name).to eq("Cruiser")
+      expect(@cruiser.length).to eq(3)
+    end
+  end
+end
