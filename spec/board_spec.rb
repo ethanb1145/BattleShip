ad9899a699d6ec2dd58121848b861dddce7497bf
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-22 15:46:53 -0600

    Begin creating 'render' spec test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -80,4 +80,12 @@ RSpec.describe Board do
       expect(@board.valid_placement?(@submarine, ["A1", "B1"])).to be(false)
     end
   end
+
+  describe "#render" do
+    it "returns a string of . , if cell has not been fired upon" do
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+      
+      expect(@board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+    end
+  end
 end
