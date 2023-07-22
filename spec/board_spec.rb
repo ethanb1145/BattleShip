a46498eec4b62bde34b84d5689b1e00e74d707bf
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-21 18:26:39 -0600

    Current progress

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Board do
   before(:each) do
     @board = Board.new
   end
+
+  describe "#initialize" do
+    it "exists" do
+      expect(@board).to be_a(Board)
+    end
+  end
+
+  describe "#cells" do
+    it "is a 4x4 board with 16 cell objects" do
+
+
+    end
+  end
 end
