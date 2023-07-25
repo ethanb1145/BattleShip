c027e11c518065d8b8b682d46920b8e7fdb8c69f
Hayden McSorley <haymcsorley@gmail.com>
2023-07-25 16:52:47 -0500

    Starting game class

---
+++ spec/game_spec.rb
@@ -0,0 +1,11 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+
+RSpec.describe Game do
+  it "exists" do
+    game = Game.new
+
+    expect(game).to be_an_instance_of(Game)
+    end
